c7568a29b13bcf776adc72b8e7b47fa4fc36c77d
Ranjit Chandramohanan <ranjit.c2000@gmail.com>
2021-04-12 17:11:30 +0530

    Calendar

--- lib/screens/attendanceCalendar.dart
+++ lib/screens/attendanceCalendar.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:table_calendar/table_calendar.dart';
-
+import 'package:syncfusion_flutter_calendar/calendar.dart';
 class AttendanceCalendar extends StatefulWidget {
   @override
   _AttendanceCalendarState createState() => _AttendanceCalendarState();
@@ -9,6 +9,34 @@ class AttendanceCalendar extends StatefulWidget {
 class _AttendanceCalendarState extends State<AttendanceCalendar> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body:SfCalendar(
+        view: CalendarView.week,
+        dataSource: MeetingDataSource(getAppointments()),
+      )
+    );
+  }
+}
+
+List<Appointment> getAppointments(){
+  List<Appointment> meetings = <Appointment>[];
+  final DateTime today = DateTime.now();
+  final DateTime startTime = 
+      DateTime(today.year,today.month,today.day,9,0,0);
+  final DateTime endTime = startTime.add(const Duration(hours:2));
+
+  meetings.add(Appointment(
+    startTime: startTime, 
+    endTime: endTime,
+    subject: 'Conference',
+    color: Colors.redAccent
+    ));
+  
+  return meetings;
+}
+class MeetingDataSource extends CalendarDataSource {
+  MeetingDataSource(List<Appointment> source){
+    appointments = source;
+
   }
 }
