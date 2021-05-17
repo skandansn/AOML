57f9413823dd235e49d0ba488c4231a1c9c63906
shakthi-stack <shakthisaravanan1@outlook.com>
2021-05-17 09:06:27 +0530

    auth&mock updates

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -2,22 +2,27 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:aumsodmll/models/user.dart';
 
 class AuthService {
-  final FirebaseAuth _auth = FirebaseAuth.instance;
-
+  FirebaseAuth auth;
+  AuthService(){
+    this.auth= FirebaseAuth.instance;
+  }
+  AuthService.instance({this.auth}){
+     auth = FirebaseAuth.instance;
+    }
   Userx _userFromFirebaseUser(User user) {
     return user != null ? Userx(uid: user.uid) : null;
   }
 
   // Stream<Userx> get user {
-  //   return _auth.authStateChanges().map(_userFromFirebaseUser);
+  //   return auth.authStateChanges().map(_userFromFirebaseUser);
   // }
   Stream<User> get user {
-    return _auth.authStateChanges();
+    return auth.authStateChanges();
   }
 
   Future signOut() async {
     try {
-      return await _auth.signOut();
+      return await auth.signOut();
     } catch (e) {
       print(e.toString());
       return null;
@@ -27,7 +32,7 @@ class AuthService {
   Future sinInWithEmailAndPassword(String email, String pass) async {
     try {
       UserCredential result =
-          await _auth.signInWithEmailAndPassword(email: email, password: pass);
+          await auth.signInWithEmailAndPassword(email: email, password: pass);
       User user = result.user;
       return _userFromFirebaseUser(user);
     } catch (e) {
@@ -38,7 +43,7 @@ class AuthService {
 
   Future<String> resetPassword(String email) async {
     try {
-      await _auth.sendPasswordResetEmail(email: email);
+      await auth.sendPasswordResetEmail(email: email);
     } catch (e) {
       return e.toString();
     }
