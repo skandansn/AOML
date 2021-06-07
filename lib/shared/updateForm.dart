7e88c74d37cf73aafb063f440be95958dcecabd6
skandansn <skandansn@protonmail.com>
2021-06-07 19:27:48 +0530

    updateForm fix2

--- lib/shared/updateForm.dart
+++ lib/shared/updateForm.dart
@@ -24,7 +24,7 @@ class _UpdateFormState extends State<UpdateForm> {
   DatabaseService _db = DatabaseService();
   @override
   Widget build(BuildContext context) {
-    final user = Provider.of<Userx>(context);
+    // final user = Provider.of<Userx>(context);
     return Form(
       key: _formKey,
       autovalidateMode: AutovalidateMode.disabled,
