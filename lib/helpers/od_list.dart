7300188444e8609198fe7370a0278ad5f6780f40
Chandra <a.chandravadhana@gmail.com>
2021-06-11 09:23:40 +0530

    1 unit test case added in od_list_test.dart

--- lib/helpers/od_list.dart
+++ lib/helpers/od_list.dart
@@ -20,51 +20,64 @@ class _ODListState extends State<ODList> {
   @override
   Widget build(BuildContext context) {
     DatabaseService _db = DatabaseService();
-    final ScrollController _scrollcontroller = ScrollController();
+    //final ScrollController _scrollcontroller = ScrollController();
     return FutureBuilder<String>(
       future: _db.getUserid(), // async work
       builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
-        switch (snapshot.connectionState) {
-          case ConnectionState.waiting:
-            return Loading();
-          default:
-            if (snapshot.hasError)
-              return Text('Error: ${snapshot.error}');
-            else {
-              final ods = Provider.of<List<OD>>(context);
-              var userid = "${snapshot.data.characters}";
-              var arr = [];
-              if (ods != null) {
-                ods.forEach((element) {
-                  if (flag == true) {
-                    if (((element.faculty == userid.toString()) ||
-                            (element.advisor == userid.toString())) &&
-                        element.steps > 0) {
-                      arr.add(element);
-                    }
-                  } else {
-                    if (element.stuid == userid.toString()) {
-                      arr.add(element);
-                    }
-                  }
-                });
-              }
-
-              return Scrollbar(
-                thickness: 10.0,
-                controller: _scrollcontroller,
-                isAlwaysShown: true,
-                child: ListView.builder(
-                  controller: _scrollcontroller,
-                  itemCount: arr.length,
-                  itemBuilder: (context, index) {
-                    return Tile(appl: arr[index], flagType: flag, sel: arg);
-                  },
-                ),
-              );
-            }
-        }
+        return showScreen_with_resp_to_ConnectionState(snapshot);
       },
     );
   }
+
+
+  showScreen_with_resp_to_ConnectionState(AsyncSnapshot<String> snapshot)
+  {
+    final ScrollController _scrollcontroller = ScrollController();
+    switch (snapshot.connectionState) {
+      case ConnectionState.waiting:
+        return Loading();
+      default:
+        if (snapshot.hasError)
+          return Text('Error: ${snapshot.error}');
+        else {
+          final ods = Provider.of<List<OD>>(context);
+          var userid = "${snapshot.data.characters}";
+          var arr = [];
+          if (ods != null) {
+            ods.forEach((element) {
+              if (flag == true) {
+                if (((element.faculty == userid.toString()) ||
+                    (element.advisor == userid.toString())) &&
+                    element.steps > 0) {
+                  arr.add(element);
+                }
+              } else {
+                if (element.stuid == userid.toString()) {
+                  arr.add(element);
+                }
+              }
+            });
+          }
+
+          return Scrollbar(
+            thickness: 10.0,
+            controller: _scrollcontroller,
+            isAlwaysShown: true,
+            child: ListView.builder(
+              controller: _scrollcontroller,
+              itemCount: arr.length,
+              itemBuilder: (context, index) {
+                // print("HELLO CHANDRA! ");
+                // print(flag);  //TRUE in case of Faculty
+                // print("HELLO CHANDRA! ");
+                // print(arg); //For now in faculty1@cb.amrita.edu arg == null
+                return Tile(appl: arr[index], flagType: flag, sel: arg);
+              },
+            ),
+          );
+        }
+    }
+  }
+
+
 }
---
+++ test/helpers/od_list_test.dart
@@ -0,0 +1,31 @@
+import 'package:aumsodmll/helpers/od_list.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:aumsodmll/mock.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:aumsodmll/shared/loading.dart';
+
+void main() {
+
+  setupFirebaseAuthMocks();
+  setUpAll(() async {
+    await Firebase.initializeApp();
+  });
+
+  test("When Connection state is waiting then load Loading screen",(){
+
+    ODList od_list = new ODList(flag: true,arg: null);
+    bool testsuccesful = false;
+    AsyncSnapshot<String> snapshot = AsyncSnapshot.waiting();
+    if(od_list.createState().showScreen_with_resp_to_ConnectionState(snapshot) is Loading)
+    {
+      testsuccesful = true;
+    }
+    else{
+      testsuccesful = false;
+    }
+
+    expect(testsuccesful,true);
+  });
+
+}
